469f63a80fe6854bd57e274bc5b481d3e92aed91
DVPervukhin <dvperv@gmail.com>
2021-01-27 11:35:06 +0300

    Controller file.meta

--- src/controllers/controller.file.meta.js
+++ src/controllers/controller.file.meta.js
@@ -13,14 +13,48 @@ module.exports = {
     },
 
     //#get the list
-    fetch: async (request, reply) => {},
+    fetch: async (request, reply) => {
+        try {
+            const metas = await modelFileMeta.find({});
+            reply.code(200).send(metas);
+        } catch (e) {
+            reply.code(500).send(e);
+        }
+    },
 
     //#get a single one
-    get: async (request, reply) => {},
+    get: async (request, reply) => {
+        try {
+            const metaId = request.params.id;
+            const meta = await modelFileMeta.findById(metaId);
+            reply.code(200).send(meta);
+        } catch (e) {
+            reply.code(500).send(e);
+        }
+    },
 
     //#update
-    update: async (request, reply) => {},
+    update: async (request, reply) => {
+        try {
+            const metaId = request.params.id;
+            const updates = request.body;
+            await modelFileMeta.findByIdAndUpdate(metaId, updates);
+            const updatedMeta = await modelFileMeta.findById(metaId);
+            reply.code(200).send({ data: updatedMeta });
+        } catch (e) {
+            reply.code(500).send(e);
+        }
+    },
 
     //#delete
-    delete: async (request, reply) => {},
+    delete: async (request, reply) => {
+        try {
+            const metaId = request.params.id;
+            const metaToDelete = await modelFileMeta.findById(metaId);
+            await modelFileMeta.findByIdAndDelete(metaId);
+            reply.code(200).send({ data: metaToDelete });
+        } catch (e) {
+            reply.code(500).send(e);
+        }
+    },
 };
