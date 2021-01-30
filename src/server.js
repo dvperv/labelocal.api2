00e0dd5e3cddc634d9d37a79127e350e60a75e25
DVPervukhin <dvperv@gmail.com>
2021-01-30 13:48:19 +0300

    Heroku prep

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ fastify.get('/file', async (request, reply) => {
 // Run the server!
 const start = async () => {
     try {
-        await fastify.listen(3000)
+        await fastify.listen(process.env.PORT || 3000)
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
