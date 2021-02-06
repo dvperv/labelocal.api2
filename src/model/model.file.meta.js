6568850f839affcf0edec935a326f0891b010764
DVPervukhin <dvperv@gmail.com>
2021-02-06 13:59:58 +0300

    backend schema update

--- src/model/model.file.meta.js
+++ src/model/model.file.meta.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const schemaFileMeta = new Schema({
-    idx: { type: String, required: true, index: true, unique: true },
+    _id: { type: String, required: true, index: true, unique: true },
     // {high/low}-#folder-#file  ex.: "high-001-000000"
     bands: { type: Number, required: true },
     points: { type: Number, required: true },
-    label: { type: String, required: true, enum: ['STAR', 'EMPTY', 'UNKNOWN', 'BLUEFIELD'] }
+    label: { type: String, required: true, enum: ['Y', 'N', 'U'] }
 });
 
 const modelFileMeta = mongoose.model('meta', schemaFileMeta);
